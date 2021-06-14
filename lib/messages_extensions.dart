f5d6ed2b8790efb50ec766137cf17e44910e10bf
Drew MacNeil <drewm@hey.com>
2021-06-13 23:32:43 -0700

    Create GlucoseSample.date() utility.

--- lib/messages_extensions.dart
+++ lib/messages_extensions.dart
@@ -1,10 +1,12 @@
 import 'package:cgmblekit_flutter/messages.dart';
 
 extension GlucoseSampleDescription on GlucoseSample {
+  DateTime date() {
+    return DateTime.fromMillisecondsSinceEpoch(this.timestamp!.round() * 1000);
+  }
+
   String description() {
-    DateTime? sampleTime = timestamp != null
-        ? DateTime.fromMillisecondsSinceEpoch(this.timestamp!.round() * 1000)
-        : null;
+    DateTime? sampleTime = timestamp != null ? date() : null;
     return '${this.quantity} mg/dL at $sampleTime';
   }
 }
